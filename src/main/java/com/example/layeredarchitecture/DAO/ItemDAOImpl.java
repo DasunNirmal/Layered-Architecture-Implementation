f64a273cea00bb67479ac27d7774e0947bdd784f
DasunNirmal <dasun200301@gmail.com>
2023-12-15 14:40:15 +0530

    Items refactor done

--- src/main/java/com/example/layeredarchitecture/DAO/ItemDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/DAO/ItemDAOImpl.java
@@ -1,8 +1,6 @@
 package com.example.layeredarchitecture.DAO;
-
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.ItemDTO;
-
 import java.sql.*;
 import java.util.ArrayList;
 
